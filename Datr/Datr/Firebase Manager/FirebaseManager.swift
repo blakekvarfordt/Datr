3e81612e6869a6bf04c804e8229462e19407c11d
Blake Kvarfordt <blakekvarfordt@gmail.com>
2020-02-29 18:16:20 -0800

    adds documentation

--- Datr/Datr/Firebase Manager/FirebaseManager.swift
+++ Datr/Datr/Firebase Manager/FirebaseManager.swift
@@ -22,8 +22,13 @@ struct FirebaseKeys {
 
 class FirebaseManager {
     
+    /// References the Firestore database
     static let databaseReference = Firestore.firestore().collection("users")
-    
+    /// Creates a Person dictionary and sends it to the databse.
+    ///
+    /// - Parameter person: The users information packaged into a first class object called Person.
+    ///
+    /// - Parameter completion: Completes with an error if there was a problem with the system. Completes with the Person object if the object was saved successfully to the database. This will be then be saved locally.
     static func addPersonToFirebase(_ person: Person, completion: @escaping (Result<Person, Error>) -> Void) {
         let personData: [String : Any] = [
             FirebaseKeys.firstName : person.firstName,
@@ -47,6 +52,11 @@ class FirebaseManager {
         }
     }
     
+     /// Checks for a user in the database that has already taken a desired username from a new user. If a user document comes back that means that the username is taken and then display an alert to the user saying to try a different username.
+    ///
+    /// - Parameter username: The new username the user wants to claim.
+    ///
+    /// - Parameter completion: Completes with an error if there was a problem with the system. Completes true  if a user document does NOT comes back. Completes false if a user document does come back.
     static func checkForUniqueUsername(_ username: String, completion: @escaping (Result<Bool, Error>) -> Void) {
         databaseReference.whereField(FirebaseKeys.username, isEqualTo: username).getDocuments { (snapshot, error) in
             if let error = error {
