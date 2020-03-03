5b59aaf2cc81c4668379d50c3e5bdef74d52a009
Blake Kvarfordt <blakekvarfordt@gmail.com>
2020-03-02 21:19:49 -0800

    adds sexual orientation data

--- Datr/Datr/Data/GenderData.swift
+++ Datr/Datr/Data/GenderData.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class GenderData {
+struct GenderData {
     static let genders: [String] = [
         "Agender",
         "Androgyne",
---
+++ Datr/Datr/Data/SexualOrientationData.swift
@@ -0,0 +1,25 @@
+//
+//  SexualOrientationData.swift
+//  Datr
+//
+//  Created by Blake kvarfordt on 3/2/20.
+//  Copyright © 2020 Blake kvarfordt. All rights reserved.
+//
+
+import Foundation
+
+struct OrientationData {
+    
+    static let sexualOrientations: [String] = [
+        "Straight",
+        "Gay",
+        "Lesbian",
+        "Bisexual",
+        "Asexual",
+        "Demisexual",
+        "Pansexual",
+        "Queer",
+        "Questioning"
+    ]
+    
+}
