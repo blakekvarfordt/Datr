cb9a0afa2055729107d6d6ea3334177d0ad2feb6
Blake Kvarfordt <blakekvarfordt@gmail.com>
2020-03-04 21:22:42 -0800

    adds string extension

---
+++ Datr/Datr/Extensions/String.swift
@@ -0,0 +1,23 @@
+//
+//  String.swift
+//  Datr
+//
+//  Created by Blake kvarfordt on 3/4/20.
+//  Copyright © 2020 Blake kvarfordt. All rights reserved.
+//
+
+import UIKit
+
+extension String {
+    func image() -> UIImage? {
+        let size = CGSize(width: 40, height: 40)
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        UIColor.clear.set()
+        let rect = CGRect(origin: .zero, size: size)
+        UIRectFill(CGRect(origin: .zero, size: size))
+        (self as AnyObject).draw(in: rect, withAttributes: [.font: UIFont.systemFont(ofSize: 40)])
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+}
--- Datr/Datr/View Controllers/PeopleViewController.swift
+++ Datr/Datr/View Controllers/PeopleViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class PeopleViewController: UIViewController {
 
+    @IBOutlet weak var cardTextView: UITextView!
+    @IBOutlet weak var resultImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-
+    @IBAction func cardAction(_ sender: UIPanGestureRecognizer) {
+        let card = sender.view!
+        let point = sender.translation(in: view)
+        let xFromCenter = card.center.x - view.center.x
+        card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
+        
+        if xFromCenter > 0 {
+            resultImageView.image = "🔥".image()
+        } else {
+            resultImageView.image = "💦".image()
+//            resultLabel.text = "💦"
+        }
+        
+        resultImageView.alpha = abs(xFromCenter) / view.center.x
+        
+        if sender.state == UIGestureRecognizer.State.ended {
+            if card.center.x < 75 {
+                UIView.animate(withDuration: 0.3, animations: {
+                    card.center = CGPoint(x: card.center.x - 200, y: card.center.y + 75)
+                    card.alpha = 0
+                })
+                return
+            } else if card.center.x > (view.frame.width - 75) {
+                UIView.animate(withDuration: 0.3, animations: {
+                    card.center = CGPoint(x: card.center.x + 200, y: card.center.y + 75)
+                    card.alpha = 0
+                })
+                return
+            }
+            
+            UIView.animate(withDuration: 0.2, animations: {
+                card.center = self.view.center
+                self.resultImageView.alpha = 0
+            })
+        }
+    }
+    
     
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
